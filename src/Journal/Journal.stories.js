7e42d7687357884463faceef5a52698682b229c1
philipsadman <philipsadman@gmail.com>
2020-07-15 17:08:22 +0300

    integration with real data

--- src/Journal/Journal.stories.js
+++ src/Journal/Journal.stories.js
@@ -3,26 +3,60 @@ import React from 'react';
 import Journal from '.';
 
 const students = [{
-  name: 'Larisa Ivanova',
-  dates: [{
-    date: '15-04-2020',
-    scores: [null, 5, null]
+  id: '11',
+  firstName: 'Rob',
+  lastName: 'Tutor',
+  events: [{
+    lessonScore: 1,
+    educationPlanEventId: '1.1',
+    testScore: 1,
+    isTestCompleted: false
   }, {
-    date: '17-04-2020',
-    scores: [null, null, 2]
+    isVisited: false,
+    educationPlanEventId: '1.2',
+    homeworkScore: 5
   }]
 }, {
-  name: 'Petr Nalich',
-  dates: [{
-    date: '16-04-2020',
-    scores: [4, 5, 3]
+  id: '10',
+  firstName: 'Boris',
+  lastName: 'Mileev',
+  events: [{
+    isVisited: false,
+    educationPlanEventId: '1.1'
   }, {
-    date: '18-04-2020',
-    scores: [4, 5, 3]
-  }, {
-    date: '20-04-2020',
-    scores: [4, 5, 3]
+    isVisited: false,
+    educationPlanEventId: '1.2',
+    homeworkScore: 3
   }]
+}];
+
+const events = [{
+  educationPlanEventId: '1.1',
+  bookedStartDate: '2020-08-03T08:35:00.000Z',
+  isCompleted: false,
+  educationPlanId: '3',
+  name: 'Lesson-1',
+  lessonId: '5',
+  testAttached: true,
+  homeworkAttached: false
+}, {
+  educationPlanEventId: '1.2',
+  bookedStartDate: '2020-08-03T08:35:00.000Z',
+  isCompleted: false,
+  educationPlanId: '4',
+  name: 'Lesson-1',
+  lessonId: '6',
+  testAttached: true,
+  homeworkAttached: true
+}, {
+  educationPlanEventId: '1.3',
+  bookedStartDate: '2020-08-03T08:35:00.000Z',
+  isCompleted: false,
+  educationPlanId: '4',
+  name: 'Lesson-1',
+  lessonId: '6',
+  testAttached: true,
+  homeworkAttached: true
 }];
 
 export default {
@@ -30,4 +64,4 @@ export default {
   component: Journal
 };
 
-export const JournalWithData = () => <Journal students={students} />;
+export const JournalWithData = () => <Journal students={students} events={events} step={2} />;
--- src/Journal/index.js
+++ src/Journal/index.js
@@ -1,41 +1,21 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
-const useNavigation = (length) => {
+const useNavigation = (length, step) => {
   const [shift, setShift] = useState(0);
-  const STEP = 4;
 
   const backward = () => shift > 0 && setShift(shift - 1);
-  const forward = () => shift < length - 4 && setShift(shift + 1);
+  const forward = () => shift < length - step && setShift(shift + 1);
 
-  const cut = (array) => {
-    return [...array].splice(shift, STEP);
+  const cut = array => {
+    return [...array].splice(shift, step);
   };
 
   return { forward, backward, cut };
 };
 
-const Journal = ({ students }) => {
-  const dates = students
-    .reduce((acc, { dates }) => {
-      dates.forEach(date => {
-        if (acc.indexOf(date.date) === -1) acc.push(date.date);
-      });
-
-      return acc;
-    }, [])
-    .sort((a, b) => {
-      const aChunks = a.split('-').map(val => parseInt(val));
-      const bChunks = b.split('-').map(val => parseInt(val));
-
-      return (
-        aChunks[2] > bChunks[2] ||
-        aChunks[1] > bChunks[1] ||
-        aChunks[0] > bChunks[0]
-      ) ? 1 : -1;
-    });
-
-  const navigation = useNavigation(dates.length);
+const Journal = ({ students, events, step }) => {
+  const navigation = useNavigation(events.length, step);
 
   const rowStyles = {
     display: 'flex',
@@ -74,8 +54,8 @@ const Journal = ({ students }) => {
           {`<`}
         </div>
 
-        {navigation.cut(dates).map(
-          (date, i) => (
+        {navigation.cut(events).map(
+          (event, i) => (
             <div
               key={i}
               style={{
@@ -85,11 +65,11 @@ const Journal = ({ students }) => {
                 height: 50
               }}
             >
-              <div style={{ flex: 1, ...rowStyles }}>{date}</div>
+              <div style={{ flex: 1, ...rowStyles }}>{event.bookedStartDate}</div>
               <div key={i} style={{ flex: 1, ...rowStyles }}>
                 <div style={{ flex: 1, ...rowStyles }}>Урок</div>
                 <div style={{ flex: 1, ...rowStyles }}>Тест</div>
-                <div style={{ flex: 1, ...rowStyles }}>Д/З</div>
+                {event.homeworkAttached && <div style={{ flex: 1, ...rowStyles }}>Д/З</div>}
               </div>
             </div>
           )
@@ -117,19 +97,29 @@ const Journal = ({ students }) => {
                 style={{ width: 300, ...rowStyles, ...firstRowStyles }}
               >
                 <span>{i + 1}</span>
-                <span style={{ marginLeft: 20 }}>{student.name}</span>
+            <span style={{ marginLeft: 20 }}>{student.firstName} {student.lastName}</span>
               </div>
 
               <div style={{ height: 50, width: 20 }}></div>
 
-              {navigation.cut(dates).map(
-                (date, i) => (
-                  <div style={{ flex: 1, ...rowStyles }} key={i}>
-                    {student.dates.find(d => d.date === date)?.scores.map(score => (
-                      <div style={{ flex: 1, ...rowStyles }}>{score}</div>
-                    ))}
-                  </div>
-                )
+              {navigation.cut(events).map(
+                (event, i) => {
+                  const studentEvent = student.events.find(
+                    e => e.educationPlanEventId === event.educationPlanEventId
+                  );
+
+                  return (
+                    <div style={{ flex: 1, ...rowStyles }} key={i}>
+                      <div style={{ flex: 1, ...rowStyles }}>{studentEvent?.lessonScore}</div>
+                      <div style={{ flex: 1, ...rowStyles }}>{studentEvent?.testScore}</div>
+                      {
+                        event.homeworkAttached && (
+                          <div style={{ flex: 1, ...rowStyles }}>{studentEvent?.homeworkScore}</div>
+                        )
+                      }
+                    </div>
+                  )
+                }
               )}
 
               <div style={{ height: 50, width: 20 }}></div>
@@ -141,18 +131,37 @@ const Journal = ({ students }) => {
   );
 };
 
-const date = PropTypes.shape({
-  date: PropTypes.string,
-  scores: PropTypes.arrayOf(PropTypes.oneOfType(PropTypes.number, null))
+const event = PropTypes.shape({
+  educationPlanEventId: PropTypes.string,
+  bookedStartDate: PropTypes.string,
+  isCompleted: PropTypes.bool,
+  educationPlanId: PropTypes.string,
+  name: PropTypes.string,
+  lessonId: PropTypes.string,
+  testAttached: PropTypes.bool,
+  homeworkAttached: PropTypes.bool
+});
+
+const scores = PropTypes.shape({
+  lessonScore: PropTypes.number,
+  isVisited: PropTypes.bool,
+  educationPlanEventId: PropTypes.string,
+  testScore: PropTypes.number,
+  isTestCompleted: PropTypes.bool,
+  homeworkScore: PropTypes.number
 });
 
 const student = PropTypes.shape({
-  name: PropTypes.string,
-  dates: PropTypes.arrayOf(date)
+  id: PropTypes.string,
+  firstName: PropTypes.string,
+  lastName: PropTypes.string,
+  events: PropTypes.arrayOf(scores)
 });
 
 Journal.propTypes = {
-  students: PropTypes.arrayOf(student).isRequired
+  students: PropTypes.arrayOf(student).isRequired,
+  events: PropTypes.arrayOf(event).isRequired,
+  step: PropTypes.number
 };
 
 export default Journal;
